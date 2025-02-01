8a6a224eb0b112240c0047b0a30d5303b3c4601c
ridoycoder007 <ridoycoder007@gmail.com>
2025-02-02 00:52:00 +0600

    check commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+export default {
+    theme: {
+      extend: {
+        colors: {
+          'primary': '#ff49db',
+        },
+        fontFamily: {
+          'sans': ['Sora', 'Helvetica', 'Arial', 'sans-serif'],
+          'adlam': ['Adlam', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
+  
